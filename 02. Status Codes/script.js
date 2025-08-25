d1d2b251e785377095f7e3ef25a422b6c5a96176
ephraim <ephraims.818@gmail.com>
2025-08-25 17:34:16 +0600

    JS--NET-NINJA---ASYNCHRONOUS-JS - 03. Callback Functions

--- 02. Status Codes/script.js
+++ 02. Status Codes/script.js
@@ -3,7 +3,10 @@
 const request = new XMLHttpRequest();
 request.addEventListener("readystatechange", () => {
     // console.log(request, request.readyState);
-    if (request.readyState === 4) {
+    if (request.readyState === 4 && request.status === 200) {
+        console.log(request);
+        console.log(request.responseText);
+    } else if (request.readyState === 4) {
         console.log(request.responseText);
     }
 });
